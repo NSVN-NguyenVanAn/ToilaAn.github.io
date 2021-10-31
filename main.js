7edb0a7a18d50ba59605abbfd0241e33393be281
toilaan <an04092001@gmail.com>
2021-10-31 08:39:54 +0700

    alo

---
+++ main.js
@@ -0,0 +1,10 @@
+var andz = 'anh an dep trai';
+// Làm bài tập tại đây
+var fullName = 'An';
+alert(fullName);
+var message = 'Học về built-in functions trong JS';
+var warnMessage = 'Đây là message cảnh báo';
+var errorMessage = 'Đây là message lỗi';
+console.log(message);
+console.warn(warnMessage);
+console.error(errorMessage);
